a32b649c8a8f43b9f4e6bae3fddb6e6c4e43ae15
chaitanyak <navchaitanya@gmail.com>
2025-04-28 02:56:58 +0530

    Added initial IAC code

--- main.tf
+++ main.tf
@@ -1,12 +1,11 @@
 resource "azurerm_storage_account" "this" {
-  name                     = var.storage_account_name
-  resource_group_name      = var.resource_group_name
-  location                 = var.location
-  account_tier             = var.account_tier
-  account_replication_type = var.account_replication_type
-  account_kind             = var.account_kind
-  access_tier              = var.account_kind == "BlobStorage" || var.account_kind == "StorageV2" ? var.access_tier : null
-  enable_https_traffic_only = true
-
-  tags = var.tags
+  name                       = var.storage_account_name
+  resource_group_name        = var.resource_group_name
+  location                   = var.location
+  account_tier               = var.account_tier
+  account_replication_type   = var.account_replication_type
+  account_kind               = var.account_kind
+  access_tier                = var.account_kind == "BlobStorage" || var.account_kind == "StorageV2" ? var.access_tier : null
+  https_traffic_only_enabled = true
+  tags                       = var.tags
 }
